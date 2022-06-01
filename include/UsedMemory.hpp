4f62858b3439040aaf5a61fe6d2d4b194502e612
melnikova.m <92054042+melnikova-maria@users.noreply.github.com>
2022-06-01 20:41:28 +0300

    Create UsedMemory.hpp

---
+++ include/UsedMemory.hpp
@@ -0,0 +1,25 @@
+// Copyright 2021 Melnikova Maria dandelion.m@mail.ru
+#ifndef INCLUDE_USEDMEMORY_HPP_
+#define INCLUDE_USEDMEMORY_HPP_
+
+#include <string>
+#include <vector>
+#include <iostream>
+
+#include "Log.hpp"
+
+class UsedMemory {
+ public:
+  void OnDataLoad(const std::vector<Item>& old_items,
+                  const std::vector<Item>& new_items);
+
+  void OnRawDataLoad(const std::vector<std::string>& old_items,
+                     const std::vector<std::string>& new_items);
+
+  [[nodiscard]] size_t Used() const;
+
+ private:
+  size_t used_ = 0;
+};
+
+#endif  // INCLUDE_USEDMEMORY_HPP_
